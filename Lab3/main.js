63fb647bea9ac92d0f8e6c85a6f3bdc2a4cd5125
Yucheng Chen <86138228+onlyPureYogurt@users.noreply.github.com>
2024-02-02 15:20:47 -0500

    move all the files to lab3 repo

---
+++ Lab3/main.js
@@ -0,0 +1,109 @@
+function loadpage(event, pageName) {
+    const sections=$('.section');
+    sections.hide();
+    const shown=$('#'+pageName);
+    shown.show();
+    $('.selected').toggleClass('selected not-selected');
+    $('#'+pageName+'-button').toggleClass('selected not-selected');
+    console.log(`page switch to ${pageName}`);
+}
+function loadProducts(category)
+{
+    const element=$('#product-selection')
+    products.forEach(product => {
+        if (category==="normal"||product.category===category)
+        {
+            element.append(
+                `<input type="checkbox" id="${product.id}" name="${product.name}" value="${product.name}">
+            <label for="${product.id}">${product.name}</label><br>`
+            )
+        }
+
+    })
+    updateProductEmpty();
+    console.log("products loaded for "+category);
+}
+
+function updateProductEmpty()
+{
+    const element=$('#product-selection')
+    if (element.children().length>0)
+    {
+        $('#select-submission').show();
+        $('#empty-sign').hide();
+        console.log("products not empty")
+    }
+    else {
+        $('#select-submission').hide();
+        $('#empty-sign').show();
+        console.log("products empty")
+    }
+}
+window.onload = function() {
+    loadProducts("normal");
+    $('#user').show();
+    $('#loading').hide();
+    $('#user-button').addClass('selected').removeClass('not-selected');
+    console.log("loaded");
+}
+function updateCartEmpty() {
+    const element = $('#cart-selection');
+    if (element.children().length > 0) {
+        $('#cart-submission').show();
+        $('#cart-empty-sign').hide();
+        console.log("Cart is not empty");
+    } else {
+        $('#cart-submission').hide();
+        $('#cart-empty-sign').show();
+        console.log("Cart is empty");
+    }
+}
+function changePreferenceType(event)
+{
+    $('#product-selection').empty();
+    $('#cart-selection').empty();
+    loadProducts(event.target.value);
+    updateCartEmpty();
+}
+
+function addProduct()
+{
+    const element=$('#product-selection');
+    const cart=$('#cart-selection');
+    element.children().each(function () {
+        if (this.checked)
+        {
+            cart.append(
+                `<input type="checkbox" id="${this.id}" name="${this.name}" value="${this.value}">
+                <label for="${this.id}">${this.name}</label><br>`
+            )
+            var label=element.find('label[for="'+this.id+'"]');
+            label.next("br").remove();
+            label.remove();
+            this.remove();
+        }
+    })
+    updateCartEmpty();
+    updateProductEmpty();
+    console.log("products added to cart");
+}
+function removeProduct(){
+    const element=$('#cart-selection');
+    const product=$('#product-selection');
+    element.children().each(function () {
+        if (this.checked)
+        {
+            product.append(
+                `<input type="checkbox" id="${this.id}" name="${this.name}" value="${this.value}">
+                <label for="${this.id}">${this.name}</label><br>`
+            )
+            var label=element.find('label[for="'+this.id+'"]');
+            label.next("br").remove();
+            label.remove();
+            this.remove();
+        }
+    })
+    updateCartEmpty();
+    updateProductEmpty();
+    console.log("products removed from cart");
+}
---
+++ Lab3/products.js
@@ -0,0 +1,12 @@
+const products = [
+    {"id": 'product-1', "name": "beef pizza", "category": "normal"},
+    {"id": 'product-2', "name": "chicken pizza", "category": "normal"},
+    {"id": 'product-3', "name": "veg pizza", "category": "vegan"},
+    {"id": 'product-4', "name": "mushroom pizza", "category": "vegan"},
+    {"id": 'product-5', "name": "pineapple pizza", "category": "normal"},
+    {"id": 'product-6', "name": "pepperoni pizza", "category": "normal"},
+    {"id": 'product-7', "name": "cheese pizza", "category": "gluten-free"},
+    {"id": 'product-8', "name": "pork pizza", "category": "gluten-free"},
+    {"id": 'product-9', "name": "sausage pizza", "category": "normal"},
+    {"id": 'product-10', "name": "plain pizza", "category": "normal"},
+];
